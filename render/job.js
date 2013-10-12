25acb5b284e690bb6b7cb680e05f4e52f4d990b2
James Halliday <mail@substack.net>
2013-10-12 11:41:02 -0700

    nested versions now render correctly, drop unused libs

--- render/job.js
+++ render/job.js
@@ -1,12 +1,14 @@
 var hyperspace = require('hyperspace');
 var fs = require('fs');
 var html = fs.readFileSync(__dirname + '/job.html', 'utf8');
+var result = require('./result.js');
 
 module.exports = function () {
     return hyperspace(html, function (row) {
         return {
             '.hash': row.key.split('.')[0],
-            '.time': new Date(row.value.time)
+            '.time': new Date(row.value.time),
+            '.results': row.value.result().pipe(result())
         };
     });
 };
---
+++ render/result.js
@@ -0,0 +1,41 @@
+var hyperspace = require('hyperspace');
+var fs = require('fs');
+var html = fs.readFileSync(__dirname + '/result.html', 'utf8');
+var through = require('through');
+var combine = require('stream-combiner');
+
+module.exports = function () {
+    var browsers = {};
+    var hs = hyperspace(html, function (row) {
+        return {
+            '.version': Object.keys(row.value).sort(cmp).map(function (v) {
+                return {
+                    _text: v,
+                    'class': row.value[v] ? 'version ok' : 'version fail'
+                };
+            })
+        };
+        
+        function cmp (a, b) {
+            var na = parseInt(a) || a;
+            var nb = parseInt(b) || b;
+            return na < nb ? -1 : 1;
+        }
+    });
+    return combine(through(write, end), hs);
+    
+    function write (row) {
+        var name = row.value.browser[0];
+        var version = row.value.browser[1];
+        if (!browsers[name]) browsers[name] = {};
+        browsers[name][version] = row.value.ok;
+    }
+    
+    function end () {
+        var tr = this;
+        Object.keys(browsers).sort().forEach(function (name) {
+            tr.queue({ key: name, value: browsers[name] });
+        });
+        tr.queue(null);
+    }
+};
