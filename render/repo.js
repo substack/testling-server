8d44fc2493269bb2355de0fc45a35cdca80ee486
James Halliday <mail@substack.net>
2013-10-11 17:50:19 -0700

    alternating row colors

--- render/repo.js
+++ render/repo.js
@@ -4,15 +4,17 @@ var html = fs.readFileSync(__dirname + '/repo.html', 'utf8');
 var job = require('./job.js');
 
 module.exports = function () {
+    var index = 0;
     return hyperspace(html, function (row) {
         return {
+            '.repo': (index++ % 2 === 0 ? { 'class': 'repo dark' } : {}),
             '.name': {
                 href: '/' + row.repo,
                 _text: row.repo
             },
             '.gravatar': { href: '/~' + row.repo.split('/')[0] },
             '.gravatar img': { src: '/avatar/' + row.user + '.jpg' },
-            '.jobs': row.jobs().pipe(job())
+            '.jobs': row.jobs({ limit: 1 }).pipe(job())
         };
     });
 };
